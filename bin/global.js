3836ea66a6738139d7c229ff112737bd4b7a6381
Scott Corgan <scottcorgan@gmail.com>
2013-10-11 16:39:25 -0700

    commander test

--- bin/global.js
+++ bin/global.js
@@ -1,7 +1,31 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
+var program = require("commander");
 
-fs.writeFile('./test.txt', 'testing', function () {
-  console.log(arguments);
-});
+
+program
+    .version("0.1.2")
+    .usage("<command> <parameters>");
+
+program
+  .command("new <name>")
+  .description("new stuff")
+  .action(function(name) {
+    fs.writeFile(__dirname + '/test.txt', name, function () {
+      console.log(arguments);
+    });
+  });
+
+program
+  .command("*")
+  .action(function() {
+    console.log([
+      "",
+      "\tCommand not found",
+      "",
+      "\tSee 'hoodie --help' for more information."
+    ].join("\n"));
+  });
+
+program.parse(process.argv);
